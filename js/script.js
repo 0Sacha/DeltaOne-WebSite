5a1cb4c9917bf40257d2320be830fc16f141736e
0Sacha <113936136+0Sacha@users.noreply.github.com>
2022-11-25 20:33:16 +0100

    darkmode 1.0

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ new ResizeObserver(entries => {
     navLinksContainer.style.transition = "none"
   }
 }).observe(document.body)
+
+
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnToggle.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+
+
+    } else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+
+    }
+
+})
